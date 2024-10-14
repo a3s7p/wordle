5d8ecc64d6eecd0d39ea462223b1d9294460ea0b
Anton <gh@gets.love>
2024-10-14 11:58:41 +0200

    add basic wordle nada program

--- nada/src/main.py
+++ nada/src/main.py
@@ -1,12 +1,23 @@
 from nada_dsl import *
 
+# the words are always 5 letters long
+NUM_LETTERS = 5
+
 def nada_main():
-    party1 = Party(name="Party1")
-    party2 = Party(name="Party2")
-    party3 = Party(name="Party3")
-    a = SecretInteger(Input(name="A", party=party1))
-    b = SecretInteger(Input(name="B", party=party2))
+    # the gamemaker sets the word of the day
+    gamemaker = Party(name="Gamemaker")
+    # the player attempts to guess the word of the day
+    player = Party(name="Player")
 
-    result = a + b
+    # have the gamemaker input 5 secret letters of the word of the day, indexes starting at 1
+    correct = [SecretInteger(Input(name = "correct_" + str(i), party = gamemaker)) for i in range(1, NUM_LETTERS + 1)]
+    # have the player input 5 secret letters of their guess of the word of the day, indexes starting at 1
+    guess = [SecretInteger(Input(name = "guess_" + str(i), party = player)) for i in range(1, NUM_LETTERS + 1)]
 
-    return [Output(result, "my_output", party3)]
+    # for each letter from word of the day and player's guess
+    # if the player's guessed letter is the correct letter: output the guessed letter at that index
+    # otherwise: output 0 for "no match"
+    output = [(g == c).if_else(g, Integer(0)) for g, c in zip(guess, correct)]
+
+    # return the output to the player
+    return [Output(o, "guess_result_" + str(i), player) for i, o in enumerate(output, 1)]
