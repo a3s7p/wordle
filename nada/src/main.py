6d5244110684e7659705ad00afe35a6c0a1a78cb
Anton <gh@gets.love>
2024-11-12 18:55:09 +0100

    nada: implement global match as negative int

--- nada/src/main.py
+++ nada/src/main.py
@@ -14,10 +14,31 @@ def nada_main():
     # have the player input 5 secret letters of their guess of the word of the day, indexes starting at 1
     guess = [SecretInteger(Input(name = "guess_" + str(i), party = player)) for i in range(1, NUM_LETTERS + 1)]
 
-    # for each letter from word of the day and player's guess
-    # if the player's guessed letter is the correct letter: output the guessed letter at that index
-    # otherwise: output 0 for "no match"
-    output = [(g == c).if_else(g, Integer(0)) for g, c in zip(guess, correct)]
+    # correct letter, right index: put positive codepoint at index
+    # else: 0 for "no local match"
+    local_match = [(g == c).if_else(c, Integer(0)) for g, c in zip(guess, correct, strict=True)]
+
+    # correct letter, wrong index: put 1 at index so the sum is non-0
+    # else: 0 for "no global match"
+    global_match = [sum([
+        (g == c2).if_else(
+            Integer(1),
+            Integer(0),
+        ) for c2 in cs
+    ]) for g, cs in zip(guess, [correct] * NUM_LETTERS, strict=True)]
+
+    outputs = [
+        # +codepoint if there is a local match, or
+        (loc == Integer(0)).if_else(
+            # -codepoint if there are >0 global matches (non-0 sum), or
+            (glo == Integer(0)).if_else(
+                # 0 for no match whatsoever
+                Integer(0),
+                g * Integer(-1),
+            ),
+            loc,
+        ) for loc, glo, g in zip(local_match, global_match, guess, strict=True)
+    ]
 
     # return the output to the player
-    return [Output(o, "guess_result_" + str(i), player) for i, o in enumerate(output, 1)]
+    return [Output(o, "guess_result_" + str(i), player) for i, o in enumerate(outputs, 1)]
