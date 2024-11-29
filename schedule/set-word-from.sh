eb94a5fa8a7311720610c4dd6f8a58a9c9d0560d
Anton <gh@gets.love>
2024-11-29 13:38:22 +0100

    fix path in set-word-from.sh

--- schedule/set-word-from.sh
+++ schedule/set-word-from.sh
@@ -4,4 +4,4 @@
 if [[ "$#" -lt 2 ]]; then echo "Usage: $0 <word list file> <line number>"; exit 1; fi
 WORDLIST="$1" OFFSET="$2"; shift 2
 WORD=$(sed '/^#/d; /^$/d' "$WORDLIST" | sed -n "${OFFSET}p")
-exec ./wordlist/set-word.sh "$WORD"
+exec ./schedule/set-word.sh "$WORD"
